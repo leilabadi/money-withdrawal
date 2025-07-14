1e961a185e4e2a9d5cc487234e73ea60929fc9fa
Mostafa <leilabadi@gmail.com>
2025-07-14 07:08:21 +0100

    Partielly added a ledger

---
+++ src/Moneybox.App/Domain/Services/ILedgerService.cs
@@ -0,0 +1,9 @@
+﻿using Moneybox.App.Domain.Model;
+
+namespace Moneybox.App.Domain.Services;
+
+public interface ILedgerService
+{
+    IList<LedgerEntry> GetAccountEntries(Guid accountId);
+    void RecordEntry(LedgerEntry entry);
+}
